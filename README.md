84faa30916878af8f41790f98a55002ac6a51b33
Alexey Poryadin <alexey.poryadin@gmail.com>
2017-06-01 01:29:07 +0300

    Published as single module

---
+++ README.md
@@ -0,0 +1,4 @@
+#tornado-unity
+
+This module help to build multiprocessing uniting tornado apps. 
+See source code and sample application for more detail.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(**{
+    'name': 'tornado-unity',
+    'version': '0.1.beta0',
+    'author': 'Alexey Poryadin',
+    'author_email': 'alexey.poryadin@gmail.com',
+    'description': 'This module help to build uniting multiprocessing tornado apps.',
+    'py_modules': ['unity'],
+    'install_requires': [
+        'tornado>=4.3',
+    ],
+    'license': 'MIT'
+})
